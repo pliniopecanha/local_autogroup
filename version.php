f59fe629fa2349796e820cc83f0633c79f4562ed
Plínio Felipe <pliniopfp@gmail.com>
2025-05-25 20:10:52 -0300

    Update version.php

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025050601;
+$plugin->version = 2025052500;
 $plugin->requires = 2013111800.00;       // Requires this Moodle version (2.7).
 $plugin->release = '4.5.1';                // Plugin release.
 $plugin->component = 'local_autogroup';  // Full name of the plugin (used for diagnostics).
