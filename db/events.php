069e796a89d020348cfd987da227104ca1cc1388
Plínio Felipe <pliniopfp@gmail.com>
2025-05-29 07:24:36 -0300

    Update events.php

--- db/events.php
+++ db/events.php
@@ -15,11 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * autogroup local plugin
- *
- * This plugin automatically assigns users to a group within any course
- * upon which they may be enrolled and which has auto-grouping
- * configured.
+ * Event observers for local_autogroup
  *
  * @package    local_autogroup
  * @copyright  Mark Ward (me@moodlemark.com)
@@ -30,124 +26,107 @@ defined('MOODLE_INTERNAL') || die();
 
 $observers = [
 
-        // Alterações em campos padrão do usuário (core profile fields como institution, department, etc)
-    [
-        'eventname'   => '\core\event\user_updated',
-        'callback'    => '\local_autogroup\event_handler::user_updated',
-    ],
-    // Alterações em campos customizados do perfil (user_info_field)
-    [
-        'eventname'   => '\core\event\user_info_field_updated',
-        'callback'    => '\local_autogroup\event_handler::user_updated',
-    ],
-    // (Opcional) Alterações em campos customizados por API
-    [
-        'eventname'   => '\core\event\user_info_field_created',
-        'callback'    => '\local_autogroup\event_handler::user_updated',
-    ],
-
-    [
-        'eventname' => '\core\event\user_enrolment_created',
-        'callback' => '\local_autogroup\event_handler::create_adhoc_task',
-        'includefile' => 'local/autogroup/classes/event_handler.php',
-        'internal' => true,
-        'priority' => 0,
-    ],
-
-    [
-        'eventname' => 'core\event\group_member_added',
-        'callback' => '\local_autogroup\event_handler::create_adhoc_task',
-        'includefile' => 'local/autogroup/classes/event_handler.php',
-        'internal' => true,
-        'priority' => 0,
-    ],
-
-    [
-        'eventname' => '\core\event\group_member_removed',
-        'callback' => '\local_autogroup\event_handler::create_adhoc_task',
-        'includefile' => 'local/autogroup/classes/event_handler.php',
-        'internal' => true,
-        'priority' => 0,
-    ],
-
+    // Detecta alterações em campos padrão do usuário (como institution, department, etc).
     [
         'eventname' => '\core\event\user_updated',
-        'callback' => '\local_autogroup\event_handler::create_adhoc_task',
+        'callback'  => '\local_autogroup\event_handler::user_updated',
+    ],
+
+    // Detecta alterações em campos customizados do perfil (user_info_field).
+    [
+        'eventname' => '\core\event\user_info_field_updated',
+        'callback'  => '\local_autogroup\event_handler::user_updated',
+    ],
+
+    // Opcional: Detecta criação de campos customizados.
+    [
+        'eventname' => '\core\event\user_info_field_created',
+        'callback'  => '\local_autogroup\event_handler::user_updated',
+    ],
+
+    // Demais eventos que criam/adicionam/alteram/removem grupos e membros.
+    [
+        'eventname' => '\core\event\user_enrolment_created',
+        'callback'  => '\local_autogroup\event_handler::create_adhoc_task',
         'includefile' => 'local/autogroup/classes/event_handler.php',
         'internal' => true,
         'priority' => 0,
     ],
-
+    [
+        'eventname' => '\core\event\group_member_added',
+        'callback'  => '\local_autogroup\event_handler::create_adhoc_task',
+        'includefile' => 'local/autogroup/classes/event_handler.php',
+        'internal' => true,
+        'priority' => 0,
+    ],
+    [
+        'eventname' => '\core\event\group_member_removed',
+        'callback'  => '\local_autogroup\event_handler::create_adhoc_task',
+        'includefile' => 'local/autogroup/classes/event_handler.php',
+        'internal' => true,
+        'priority' => 0,
+    ],
     [
         'eventname' => '\core\event\group_created',
-        'callback' => '\local_autogroup\event_handler::create_adhoc_task',
+        'callback'  => '\local_autogroup\event_handler::create_adhoc_task',
         'includefile' => 'local/autogroup/classes/event_handler.php',
         'internal' => true,
         'priority' => 0,
     ],
-
     [
         'eventname' => '\core\event\group_deleted',
-        'callback' => '\local_autogroup\event_handler::create_adhoc_task',
+        'callback'  => '\local_autogroup\event_handler::create_adhoc_task',
         'includefile' => 'local/autogroup/classes/event_handler.php',
         'internal' => true,
         'priority' => 0,
     ],
-
     [
         'eventname' => '\core\event\group_updated',
-        'callback' => '\local_autogroup\event_handler::create_adhoc_task',
+        'callback'  => '\local_autogroup\event_handler::create_adhoc_task',
         'includefile' => 'local/autogroup/classes/event_handler.php',
         'internal' => true,
         'priority' => 0,
     ],
-
     [
         'eventname' => '\core\event\role_assigned',
-        'callback' => '\local_autogroup\event_handler::create_adhoc_task',
+        'callback'  => '\local_autogroup\event_handler::create_adhoc_task',
         'includefile' => 'local/autogroup/classes/event_handler.php',
         'internal' => true,
         'priority' => 0,
     ],
-
     [
         'eventname' => '\core\event\role_unassigned',
-        'callback' => '\local_autogroup\event_handler::create_adhoc_task',
+        'callback'  => '\local_autogroup\event_handler::create_adhoc_task',
         'includefile' => 'local/autogroup/classes/event_handler.php',
         'internal' => true,
         'priority' => 0,
     ],
-
     [
         'eventname' => '\core\event\role_deleted',
-        'callback' => '\local_autogroup\event_handler::create_adhoc_task',
+        'callback'  => '\local_autogroup\event_handler::create_adhoc_task',
         'includefile' => 'local/autogroup/classes/event_handler.php',
         'internal' => true,
         'priority' => 0,
     ],
-
     [
         'eventname' => '\core\event\course_created',
-        'callback' => '\local_autogroup\event_handler::create_adhoc_task',
+        'callback'  => '\local_autogroup\event_handler::create_adhoc_task',
         'includefile' => 'local/autogroup/classes/event_handler.php',
         'internal' => true,
         'priority' => 0,
     ],
-
     [
         'eventname' => '\core\event\course_restored',
-        'callback' => '\local_autogroup\event_handler::create_adhoc_task',
+        'callback'  => '\local_autogroup\event_handler::create_adhoc_task',
         'includefile' => 'local/autogroup/classes/event_handler.php',
         'internal' => true,
         'priority' => 0,
     ],
-
     [
         'eventname' => '\totara_core\event\position_updated',
-        'callback' => '\local_autogroup\event_handler::create_adhoc_task',
+        'callback'  => '\local_autogroup\event_handler::create_adhoc_task',
         'includefile' => 'local/autogroup/classes/event_handler.php',
         'internal' => true,
         'priority' => 0,
-    ]
-
+    ],
 ];
